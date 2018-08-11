89f83c86393a878f586a122a98fec09127554359
Your Name <you@example.com>
2018-08-11 15:25:21 +0800

    1

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React,{Component} from 'react';
+import {Button} from  'antd-mobile'
+class  App extends  Component{
+    render(){
+        return (/*<h1>fsdjgjdfh</h1>*/<Button type='primary'></Button>)
+
+    }
+}
+export  default App;
